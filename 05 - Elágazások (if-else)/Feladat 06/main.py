b0bad9de8cd67091489ed6ff3f5f406bc959a2a1
hedi113 <b1hodhed@vasvari.org>
2022-11-11 12:31:27 +0100

    valami

--- 05 - Elágazások (if-else)/Feladat 06/main.py
+++ 05 - Elágazások (if-else)/Feladat 06/main.py
@@ -13,4 +13,12 @@ szam3 = int(input())
 
 if (szam1 > szam2 and szam3):
     if (szam2 > szam3):
-        print(f"{szam1},")
+        print(f"{szam1}, {szam2}, {szam3}")
+
+if (szam2 > szam1 and szam3):
+    if (szam1 > szam3):
+        print(f"{szam2}, {szam1}, {szam3}")
+
+if (szam3 > szam1 and szam2):
+    if(szam1 > szam2):
+        print(f"{szam3}, {szam1}, {szam2}")
