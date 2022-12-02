7e6079f3c43ee92645f8fadee789e608f6edabe2
hedi113 <b1hodhed@vasvari.org>
2022-12-02 07:16:50 +0100

    feladat

--- 06 - Elágazások (switch-case)/Feladat 05/main.py
+++ 06 - Elágazások (switch-case)/Feladat 05/main.py
@@ -3,13 +3,13 @@ r2: int = None
 kotes: str = None
 r1 = int(input("Kérem az R1 értékét: "))
 r2 = int(input("Kérem az R2 értékét: "))
-kotes = input("Kérem a kötés típusát: ")
+kotes = str(input("Kérem a kötés típusát: "))
 
 match kotes:
-    case "pP":
+    case "p"| "P":
         eredmeny = (r1+r2)/(r1*r2)
-        print(f"A soros kötés ellenállása. {eredmeny}")
-    case "sS":
+        print(f"A soros kötés ellenállása: {eredmeny}")
+    case "s"| "S":
         eredmeny2 = r1 + r2
         print(f"A soros kötés ellenállása: {eredmeny2}")
     case _:
