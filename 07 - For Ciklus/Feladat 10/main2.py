19437794190126780a6635827750a7e11da7bebe
hedi113 <b1hodhed@vasvari.org>
2022-12-14 07:48:39 +0100

    feladat

--- 07 - For Ciklus/Feladat 10/main2.py
+++ 07 - For Ciklus/Feladat 10/main2.py
@@ -1,11 +1,13 @@
 kezdoertek: int = None
 vegertek: int = None
+osszeg: int = None
 
 print("Kérem a kezdő értéket: ", end = "")
 kezdoertek = int(input())
 print("Kérem a végső értéket: ", end = "")
 vegertek = int(input())
 
-if(kezdoertek != 0):
-    for i in range(kezdoertek, vegertek, -2):
-        print(i)
+osszeg = vegertek
+for i in range(kezdoertek, vegertek):
+        osszeg = osszeg * i
+        print(osszeg)
