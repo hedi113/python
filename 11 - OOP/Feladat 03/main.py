6148f4f042c57c28a02db7f07dcce2ac113795ab
hedi113 <b1hodhed@vasvari.org>
2023-05-03 07:52:46 +0200

    2023.05.03

---
+++ 11 - OOP/Feladat 03/main.py
@@ -0,0 +1,14 @@
+from motorcycle import Motorcycle
+
+hornet: Motorcycle = Motorcycle()
+
+hornet.cylinders = 4
+hornet.fuelConsumption = 5
+hornet.horsePower = 102
+hornet.manufacture = "Honda"
+hornet.model = "CB 600F Hornet"
+hornet.productionYear = 2008
+hornet.torgue = 180
+hornet.type = "Naked"
+
+print(f"{hornet}")
---
+++ 11 - OOP/Feladat 03/motorcycle.py
@@ -0,0 +1,15 @@
+class Motorcycle:
+    def __init__(self):
+        super().__init__()
+        self.manufacture: str
+        self.type: str = None
+        self.model: str = None
+        self.fuelConsumption: float = 0
+        self.productionYear: int = 0
+        self.horsePower: int = 0
+        self.cylinders: int = 0
+        self.torgue: int = 0
+    
+    #magic methods
+    def __str__(self) -> str:
+        return f"{self.manufacture} {self.model} ({self.productionYear})"
