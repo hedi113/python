ea2bd7541be622e4a7aba613c31266f0a7675b72
hedi113 <hodirovo.hedi@gmail.com>
2023-05-11 08:50:28 +0200

    2023.05.11/otthoni

---
+++ 11 - OOP/Feladat 05/data.py
@@ -0,0 +1,12 @@
+class LegoSet:
+
+        def __init__(self) -> None:
+            super().__init__()
+            self.pieces: int = 0
+            self.buidTime: str = None
+            self.number: int = 78612754
+            self.name: str = None
+            self.ageGroup: str = None
+
+        def __str__(self) -> str:
+              return f"A szett azonosítója: {self.number}\n Neve: {self.name}\n Darabszám: {self.pieces}\n Építésre szükséges min. idő: {self.buidTime}\n Korosztály: {self.ageGroup}"
---
+++ 11 - OOP/Feladat 05/main.py
@@ -0,0 +1,11 @@
+from data import LegoSet
+
+legoSet: LegoSet = LegoSet()
+
+legoSet.number: 78612754
+legoSet.name = "Ochidea"
+legoSet.buidTime = "4h"
+legoSet.pieces = 200
+legoSet.ageGroup = "12 és felette"
+
+print(legoSet)
